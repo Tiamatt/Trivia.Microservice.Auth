4b80d2f8b4e24dea8ef80db34f95261ba227db78
tiamatt <samira.yusifova@gmail.com>
2019-06-16 20:01:59 -0400

    Add Auth.Migration project. Add test migration

---
+++ Auth.Migration/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Initialization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Auth.Migration
+{
+    class Program
+    {
+        public IConfiguration Configuration { get; }
+
+        static void Main(string[] args)
+        {
+            var serviceProvider = CreateServices();
+
+            // Put the database update into a scope to ensure
+            // that all resources will be disposed.
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        // Configure the dependency injection services
+        private static IServiceProvider CreateServices()
+        {
+            // kali - move to app settings
+            string connectionString = "Server=DESKTOP-O93KUA7\\SQLEXPRESS;Database=Trivia.AuthDb;User Id=TriviaAuthDb_Admin;Password=Tr1v1a20!9;Trusted_Connection=True;";
+
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // Add SQLite support to FluentMigrator
+                    .AddSqlServer()
+                    // Set the connection string
+                    .WithGlobalConnectionString(connectionString)
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(CreateTable_test).Assembly).For.Migrations()) // kali - make it dynamic
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+        }
+    }
+}
---
+++ Auth.Migration/README.md
@@ -0,0 +1,12 @@
+﻿## How to run migration
+1. Set "Auth.Migration" as a StartUp project
+2. Run the app in debug mode
+
+## Results
+1. Go to your database
+2. If [dbo].[VersionInfo] table never existed before, the app creates it
+3. Check your migrations results in database
+
+## Note
+1. If for some reasons you need to make your migration script run again, then
+remove target version from [dbo].[VersionInfo] table and run "Auth.Migration" again
